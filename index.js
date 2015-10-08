1e9d86bbcffe4e49186bcf97ba0c91f569b4f2c0
Pavel Aks <pavel.aks@gmail.com>
2015-10-08 08:45:46 +0300

    stage-3
    First Module (one function)

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var http = require("http");
 var fs = require("fs");
 
+var my_module = require("./my_module.js");
+
 http.createServer(function(request, response) {
 	console.log("Wow! New connection!");
+	
+	//check module
+	my_module();
+	
 	response.writeHead(200, {"Content-Type": "text/html"});
 	fs.readFile('app.html', function (err, data) {
 		  if (err){console.log("Ошибка чтения файла")};
---
+++ my_module.js
@@ -0,0 +1,4 @@
+var hello = function() {
+  console.log("Hello! From module...");						
+}
+module.exports = hello;
